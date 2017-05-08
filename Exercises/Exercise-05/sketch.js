da87528627ef37fc7bce946f7abdabc395dcbf25
shrutigunda <shrutigunda@cca.edu>
2017-05-08 16:40:06 -0700

    kjnj

--- Exercises/Exercise-05/sketch.js
+++ Exercises/Exercise-05/sketch.js
@@ -80,6 +80,9 @@ function setup(){
 }
 
 function draw(){	
+textFont(font5);
+text("Click anywhere on screen.", 25, 25, 75, 50);
+
 	var imageArray = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img21, img22, img23, img24, img25, img19, img20];
 	var textArray = [text1,text2, text3, text4, text5, text6, text7, text8, text9, text10, text11, text12, text13, text14, text15, text16, text17, text18, text19, text20];
 
